b0c1d9013346679b3b117f5725b47e9a8abfe37a
LHIMER Hamza <hamza.lhimer@soprasteria.com>
2021-07-02 14:22:55 +0200

    feat(env): change header router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import ROUTES from './constants/routes';
 
 const App = () => {
   return (
-    <Switch>
-      {ROUTES.map(route => (
-        <Route key={route} {...route} />
-      ))}
-    </Switch>
+    <>
+      <Switch>
+        {ROUTES.map(route => (
+          <Route key={route} {...route} />
+        ))}
+      </Switch>
+    </>
   );
 };
 
