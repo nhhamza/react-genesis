2299ba7534d0b7ea213294aba3873588ab2e5e9c
EMEAAD\tcourteaux <thomas.courteaux@ext.soprasteria.com>
2021-07-06 09:38:58 +0200

    ajout de la route

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -1,9 +1,11 @@
 // import Home from '../containers/Home/Home';
 import SearchResults from '../containers/SearchResults/SearchResults';
+import Search from '../containers/Search/Search';
 import ROUTES_CONSTANTS from './route.constants';
 
 const ROUTES = [
-  { exact: true, path: ROUTES_CONSTANTS.research, component: SearchResults }
+  { exact: true, path: ROUTES_CONSTANTS.research, component: SearchResults },
+  { exact: true, path: ROUTES_CONSTANTS.researchBase, component: Search }
 ];
 
 export default ROUTES;
