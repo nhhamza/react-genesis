aa4d55224ccc93ae5d465f1054ee7e344e814708
LHIMER Hamza <hamza.lhimer@soprasteria.com>
2021-07-07 10:25:17 +0200

    feat(env):correct suite tests.

--- src/api/api.conf.jsx
+++ src/api/api.conf.jsx
@@ -1,6 +1,6 @@
 let envVariables = {};
-
+const env = process.env.ENVIRONMENT || 'lcl';
 /* eslint-disable */
-envVariables = require(`../../.env.${process.env.ENVIRONMENT}.js`);
+envVariables = require(`../../.env.${env}.js`);
 
 export const { API_ROOT } = envVariables;
