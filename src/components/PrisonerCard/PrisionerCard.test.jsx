d7e9ce33e05ef8cf11c4701a5929720002914736
LHIMER Hamza <hamza.lhimer@soprasteria.com>
2021-07-08 15:11:21 +0200

    feat(env): add search autocomplete.

---
+++ src/components/PrisonerCard/PrisionerCard.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PrisionerCard from './PrisionerCard.jsx';
+
+describe('PrisionerCard component', () => {
+  const MOCK_PROPS = {
+    card: { id: 'id', title: 'title' }
+  };
+
+  const wrapper = shallow(<PrisionerCard {...MOCK_PROPS} />);
+  it('should render', () => {
+    expect(wrapper.exists()).toMatchSnapshot();
+  });
+});
