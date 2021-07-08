a1a8524abf96ee46d4b03c9c39ddf8b563701da1
EMEAAD\tcourteaux <thomas.courteaux@ext.soprasteria.com>
2021-07-08 17:31:23 +0200

    Maj du composant de recherche

--- src/containers/Search/Search.jsx
+++ src/containers/Search/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import './style.scss';
+import SearchIcon from '@material-ui/icons/Search';
 import logoMJ from '../../assets/images/logo_MJ.png';
 import logoDAP from '../../assets/images/logo_dap.png';
 import t from '../../constants/translates';
@@ -15,11 +16,16 @@ const Search = () => {
           </div>
         </div>
         <div className="input-component">
+          <SearchIcon />
           <input
             className="input-component__input"
             type="text"
             placeholder={t.search.placeholder}
           />
+          <select id="input-component__select" name="input-component__select">
+            <option value="detenu">Détenu</option>
+            <option value="UGC">UGC</option>
+          </select>
         </div>
         <div className="logo-container">
           <img
